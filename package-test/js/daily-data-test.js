d084f29bf484e8ffb50dc3b49fbadf1396f57aac
Ashley Davis <ashley@codecapers.com.au>
2017-10-05 09:36:06 +1000

    Added js test module to test the package works from JavaScript.

---
+++ package-test/js/daily-data-test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var AlphaVantageAPI = require('alpha-vantage-cli').AlphaVantageAPI;
+
+var yourApiKey = 'demo';
+var alphaVantageAPI = new AlphaVantageAPI(yourApiKey, 'compact', true);
+
+alphaVantageAPI.getDailyData('MSFT')
+    .then(dailyData => {
+        console.log("Daily data:");
+        console.log(dailyData);
+    });
---
+++ package-test/js/intraday-data-test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var AlphaVantageAPI = require('alpha-vantage-cli').AlphaVantageAPI;
+
+var yourApiKey = 'demo';
+var alphaVantageAPI = new AlphaVantageAPI(yourApiKey, 'compact', true);
+
+alphaVantageAPI.getIntradayData('MSFT', '15min')
+    .then(intradayData => {
+        console.log("Intraday data:");
+        console.log(intradayData);
+    });
